5e039557b981ea8118b39378c482829f0cebde5d
Olivia Pearce <oliviapearce@hotmail.co.uk>
2023-11-18 21:24:51 +0000

    added post html and function

--- myaande/views.py
+++ myaande/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
 
 # Create your views here.
@@ -12,6 +12,13 @@ def get_myaande(request):
     return render(request, 'myaande/aande.html', collection)
 
 def post_myaande(request):
+    if request.method == 'POST':
+        title = request.POST.get('title_name')
+        description = request.POST.get('description_name')
+        dateposted = request.POST.get('date_name')
+        Post.objects.create(title=title, description=description, dateposted=dateposted)
+
+        return redirect('seeposts')
     return render(request, 'myaande/add_post.html')
 
 
