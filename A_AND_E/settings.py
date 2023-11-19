1469172c7da9924594695a246691529c9a9fe79a
Olivia Pearce <oliviapearce@hotmail.co.uk>
2023-11-19 16:39:55 +0000

    edited allowed hosts to fix deployment bug

--- A_AND_E/settings.py
+++ A_AND_E/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['anything-and-everything-be0c15127928.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['anything-and-everything.herokuapp.com', 'localhost']
 
 
 # Application definition
