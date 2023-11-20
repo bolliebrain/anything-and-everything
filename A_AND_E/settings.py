a4d2ca1c5c116284891830f735e3955c9dc2116c
Olivia Pearce <oliviapearce@hotmail.co.uk>
2023-11-20 19:16:00 +0000

    installed allauth

--- A_AND_E/settings.py
+++ A_AND_E/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['anything-and-everything-be0c15127928.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['anything-and-everything-be0c15127928.herokuapp.com', 'localhost', '8000-bolliebrain-anythingand-omczdn6o7cd.ws-eu106.gitpod.io']
 
 
 # Application definition
@@ -40,11 +40,22 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
-    'myaande'
+    'myaande',
+    'accounts',
 ]
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -54,6 +65,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'A_AND_E.urls'
