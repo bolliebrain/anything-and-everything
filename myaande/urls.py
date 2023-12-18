f1dfc123f39a92e19615aaa4b065bafcc7cdf117
Olivia Pearce <oliviapearce@hotmail.co.uk>
2023-12-18 22:24:59 +0000

    added url to edit comment and delete omment

--- myaande/urls.py
+++ myaande/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('addapost', PostAande.as_view(), name='addpost'),
     path('<slug:slug>/edit', EditAande.as_view(), name='edit_post'),
     path('<slug:slug>/delete', DeleteAande.as_view(), name='delete_post'),
+    path('editcomment', EditComment.as_view(), name='edit_comment'),
+    path('<slug:slug>/deletecomment', DeleteComment.as_view(), name='delete_comment')
 ]
-
